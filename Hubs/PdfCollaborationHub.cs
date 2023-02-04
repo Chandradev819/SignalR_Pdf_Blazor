42c684e82a7802c4b1b2a1714b6a9942417c0db3
Chandradev Prasad <chandradev.prasad@channelmechanics.com>
2023-02-05 00:24:54 +0530

    Add project files.

---
+++ Hubs/PdfCollaborationHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+namespace SignalR_Pdf_Blazor.Hubs
+{
+    public class PdfCollaborationHub : Hub
+    {
+        public async Task SendPdfCommand(string command)
+        {
+            await Clients.All.SendAsync("ReceivePdfCommand", command);
+        }
+    }
+}
---
+++ wwwroot/Js/AdobePdf.js
@@ -0,0 +1,18 @@
+﻿window.adobePdfFunctions = {
+	adobePdfViwer: function (pdfFilePath) {
+		var adobeDCView = new AdobeDC.View({ clientId: "5eae34846d994ac0b6d671f8c316994f", divId: "adobe-dc-view" });
+		adobeDCView.previewFile({
+			content: { location: { url: pdfFilePath } },
+			metaData: { fileName: "Bodea Brochure.pdf" }
+		}, { showAnnotationTools: true, showDownloadPDF: true, showPrintPDF: true });
+	}
+};
+
+document.addEventListener("adobe_dc_view_sdk.ready", function () {
+	var adobeDCView = new AdobeDC.View({ clientId: "5eae34846d994ac0b6d671f8c316994f", divId: "adobe-dc-view" });
+	adobeDCView.previewFile({
+		content: { location: { url: "https://documentservices.adobe.com/view-sdk-demo/PDFs/Bodea Brochure.pdf" } },
+		metaData: { fileName: "Bodea Brochure.pdf" }
+	}, {});
+});
+
